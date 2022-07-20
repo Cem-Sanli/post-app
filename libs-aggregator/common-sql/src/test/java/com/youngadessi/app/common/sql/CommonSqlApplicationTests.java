f3db03078b59c3c73aafde138fd53de64766e0c5
Cem-Sanli <81990759+Cem-Sanli@users.noreply.github.com>
2022-07-20 14:43:42 +0300

    skeleton

---
+++ libs-aggregator/common-sql/src/test/java/com/youngadessi/app/common/sql/CommonSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngadessi.app.common.sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
